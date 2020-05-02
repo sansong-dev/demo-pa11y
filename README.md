2e2db74fe3bca933e6be3d6d49a4e8ff70577ddd
Sophia Ansong <AnsongS@ebrd.com>
2020-05-02 15:01:11 +0100

    Update readme and gitignore file

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# pa11y-example-project
+# Pa11y Sample Project
+
 Example project demonstrating accessibility testing using pa11y
+
+
+Accessibility test will be executed against [The Before and After Demonstration (BAD)](https://www.w3.org/WAI/demos/bad/Overview.html)  website from W3C WAI
--- index.js
+++ index.js
@@ -4,11 +4,17 @@ const fs = require('fs');
 const pa11y = require('pa11y');
 const htmlReporter = require('pa11y-reporter-html');
 const targetUrl = 'https://www.w3.org/WAI/demos/bad/before/news.html';
+const reportsDir = "reports";
+
+if (!fs.existsSync(reportsDir))
+    fs.mkdirSync(reportsDir);
 
 runAccessibilityTest();
 
 async function runAccessibilityTest() {
+
     try {
+
         const result = await pa11y(targetUrl, {
 
             // Run some actions before the tests e.g. login to the site
@@ -20,7 +26,7 @@ async function runAccessibilityTest() {
                 ],
 
             //Take a screenshot after running above actions
-            screenCapture: './screenshot.png',
+            screenCapture: `${reportsDir}/screenshot.png`,
 
             // Log what's happening to the console
             log: {
@@ -32,7 +38,7 @@ async function runAccessibilityTest() {
 
         const html = await htmlReporter.results(result);
 
-        fs.writeFile('./pa11y-test-report.html', html, (err) => {
+        fs.writeFile(`${reportsDir}/pa11y-test-report.html`, html, (err) => {
              if (err) throw err;
         })
 
